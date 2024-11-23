682e5999a1d9eaede92e2aebf9ca2e22498d0d5b
MartinCamerino <droldcm31@gmail.com>
2024-11-23 09:16:36 -0500

    Updated AuthMiddleware!

--- src/middleware.php
+++ src/middleware.php
@@ -1,12 +1,4 @@
 <?php
 
-use Slim\Middleware\HttpBasicAuthentication;
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
-
-// Create the Middleware instants
 $authMiddleware = new AuthMiddleware();
-
-// Add the API Key middleware to the Slim application
 $app->add($authMiddleware);    
